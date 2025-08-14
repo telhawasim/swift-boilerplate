4e3fe109863550313b33b7bde6fe240a31761936
Telha Wasim <telhawasim@gmail.com>
2025-08-14 19:21:21 +0500

    first commit

---
+++ swift-boilerplate/swift_boilerplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_boilerplateApp.swift
+//  swift-boilerplate
+//
+//  Created by Telha Wasim on 14/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct swift_boilerplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
